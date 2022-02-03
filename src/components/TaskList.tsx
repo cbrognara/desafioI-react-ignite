71d5ca58a5f4316f25f4cb750aad62a31dabc156
Camila Brognara <cbrognara@gmail.com>
2022-02-03 16:01:01 -0300

    foi

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-
+import { useDebugValue, useState } from 'react'
 import '../styles/tasklist.scss'
-
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
@@ -16,14 +14,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    setTasks(tasks => [...tasks, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => task.id === id ? {...task, isComplete: !task.isComplete} : task)
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removedTask = tasks.filter(task => task.id != id)
+    setTasks(removedTask)
   }
 
   return (
@@ -42,7 +55,7 @@ export function TaskList() {
             <FiCheckSquare size={16} color="#fff"/>
           </button>
         </div>
-      </header>
+      </header>git
 
       <main>
         <ul>
